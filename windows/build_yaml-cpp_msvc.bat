60454aa17024b804278792e18a9e5ff135a709f0
KimBomm <springnode@gmail.com>
2021-09-16 01:54:17 +0900

    add yamlcpp

--- windows/build_yaml-cpp_msvc.bat
+++ windows/build_yaml-cpp_msvc.bat
@@ -1,18 +1,22 @@
 @echo off
-set version=0.6.3
+set version=0.7.0
 set name=%~n0
+title %name%
 call :SafeRMDIR %name%
 mkdir %name%
 cd %name%
-curl -L "https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-%version%.zip" -o "yaml-cpp-%version%.zip"
+
+curl -L "https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-%version%.zip" -o "yaml-cpp-%version%.zip"
 7z x "yaml-cpp-%version%.zip" -y
 mkdir build
 cd build
 cmake ../yaml-cpp-yaml-cpp-%version%^
- -G "Visual Studio 15 2017 Win64"^
+ -G "Visual Studio 16 2019"^
  -DCMAKE_BUILD_TYPE=RELEASE^
  -DCMAKE_INSTALL_PREFIX=build^
- -DBUILD_SHARED_LIBS:BOOL=ON
+ -DYAML_BUILD_SHARED_LIBS=ON^
+ -DYAML_CPP_BUILD_TESTS=OFF^
+ -DBUILD_TEST=OFF^
 ;
 
 cmake --build . --config Release --target ALL_BUILD
